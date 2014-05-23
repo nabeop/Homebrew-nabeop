c8445d219682e8994d5386ca8dc7efba7043d274
nabeop <Kota@Aquila.local>
2014-05-23 11:11:27 +0900

    removed adobe-reader-unilung.rb

--- Casks/adobe-reader-unilung.rb
+++
@@ -1,8 +0,0 @@
-class AdobeReaderUnilung < Cask
-  url 'http://ardownload.adobe.com/pub/adobe/reader/mac/11.x/11.0.06/misc/AdbeRdrUpd11006.dmg'
-  homepage 'http://www.adobe.com/products/reader.html'
-  version '11.0.06'
-  sha256 'ae4f889b68729f2948cc80859012241b166d6efbf4113e63d5df4297531383f7'
-  install 'AdbeRdrUpd11006.pkg'
-  uninstall :pkgutil => 'com.adobe.acrobat.reader.11006.*'
-end

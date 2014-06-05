0975141266bd3bd517f43069aad93432a9d8e53a
nabeop <Kota@Aquila.local>
2014-06-05 10:10:20 +0900

    add cask

---
+++ Casks/mutecon.rb
@@ -0,0 +1,7 @@
+class Mutecon < Cask
+  url 'http://www.sara-mac.com/fw/mutec202.dmg'
+  homepage 'http://www.homepage1.nifty.com/macbs/index.html'
+  version 'Latest'
+  sha256 '557719f674270523c3ba75f3f57e51987cf132037a7e7b6a780b63e4f0657ab6'
+  link 'Mutecon.app'
+end

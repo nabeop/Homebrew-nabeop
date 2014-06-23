edccb8e4de863b135711d467fa7c3b690aa10981
nabeop <kt.mynb@gmail.com>
2014-06-23 19:15:28 +0900

    remove mjograph.rb

--- Casks/mjograph.rb
+++
@@ -1,9 +0,0 @@
-class Mjograph < Cask
-  version '4.6.1'
-  sha256 '82e495df2a779b2ee2b74142c7ab95eb63f860ca825d690a78eac83c3ea98da3'
-
-  url 'http://downloads.sourceforge.net/project/mjograph/mjograph/mjograph-4.6.1/mjogpragh-for-osx-4.6.1.zip'
-  homepage 'http://www.ochiailab.dnj.ynu.ac.jp/mjograph/indexj.html'
-
-  link 'Mjograph.app'
-end

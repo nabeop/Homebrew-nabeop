f6e8655daad22a2797e58a21f959c6eec5f1ded7
nabeop <kt.mynb@gmail.com>
2014-08-06 16:14:29 +0900

    update for new version macvim-kaoriya

--- Casks/macvim-kaoriya.rb
+++ Casks/macvim-kaoriya.rb
@@ -1,6 +1,6 @@
 class MacvimKaoriya < Cask
-  url 'https://github.com/splhack/macvim/releases/download/20140731/macvim-kaoriya-20140731.dmg'
+  url 'https://github.com/splhack/macvim/releases/download/20140805/macvim-kaoriya-20140805.dmg'
   version 'latest'
-  sha256 '474e824dfcbc562170f99d3603080741b98418aad5f0e7ba74ae189526375bc5'
+  sha256 'a47e42e32ddb3dd26e65e1ccfac532474da2a89fb516eb3a480b4f0ddabe4235'
   link 'MacVim.app'
 end

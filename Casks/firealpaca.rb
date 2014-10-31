ebbbfae2324bacdf386073e2d725c46f5205fc20
nabeop <kt.mynb@gmail.com>
2014-10-31 14:56:56 +0900

    add firealpaca cask

---
+++ Casks/firealpaca.rb
@@ -0,0 +1,7 @@
+class FireAlpaca < Cask
+	version 'latest'
+	sha256 'nocheck'
+	url 'http://firealpaca.com/download.php?os=mac&key=17813449013210197561d4f66c5aca8c'
+	homepacge 'http://firealpaca.com'
+	link 'FireAlpaca.app'
+end

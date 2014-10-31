69248d23e7f06261c826f14908928a655c5099f9
nabeop <kt.mynb@gmail.com>
2014-10-31 15:03:56 +0900

    remove firealpaca

--- Casks/firealpaca.rb
+++
@@ -1,7 +0,0 @@
-class FireAlpaca < Cask
-	version 'latest'
-	sha256 'nocheck'
-	url 'http://firealpaca.com/download.php?os=mac&key=17813449013210197561d4f66c5aca8c'
-	homepage 'http://firealpaca.com'
-	link 'FireAlpaca.app'
-end
